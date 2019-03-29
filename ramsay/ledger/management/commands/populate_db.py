622f8d46e83c3dc260375faf991cb652a2972dae
TeaOfJay <TeaOfJay@users.noreply.github.com>
2019-03-28 23:35:59 -0500

    fixed Carter's module import error, just missing example csv

--- ramsay/ledger/management/commands/populate_db.py
+++ ramsay/ledger/management/commands/populate_db.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from ramsay.ledger.models import Ledger, Citation
+from ledger.models import Ledger, Citation
 import pandas
 
 class Command(BaseCommand):
