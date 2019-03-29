2b7a020f9a6b1cf7225a5fb1cfc25f53f4bdbcbd
TeaOfJay <TeaOfJay@users.noreply.github.com>
2019-03-29 18:57:54 -0500

    Working website completing most iteration features

--- ramsay/ledger/models.py
+++ ramsay/ledger/models.py
@@ -31,12 +31,12 @@ class Product(models.Model):
 	image     = models.ImageField(upload_to='ledger', blank=True, null=True)
 
 	materials = models.CharField(max_length=255 , blank=True, null=True)
-	#dimensions =
+	dimensions = models.CharField(max_length=255, blank=True, null=True)
 
 
 	origin    = models.CharField(max_length=64, blank=True, null=True)
 	collection = models.CharField(max_length=255, blank=True, null=True)
-	link      = models.CharField(max_length=255)
+	link      = models.CharField(max_length=255, blank=True, null=True)
 
 	origin_description = models.TextField(blank=True, null=True)
 	production_country = models.CharField(max_length=64, blank=True, null=True)
@@ -44,19 +44,18 @@ class Product(models.Model):
 	materials_location = models.CharField(max_length=64, blank=True, null=True)
 	description        = models.TextField(blank=True, null=True)
 
-	license   = models.CharField(max_length=64) #can limit to specific choices
+	license   = models.CharField(max_length=64, blank=True, null=True) #can limit to specific choices
 
-	license_link = models.CharField(max_length=255)
+	license_link = models.CharField(max_length=255, blank=True, null=True)
 
 
 	#unit_number = models.IntegerField(default=0)
 	#unit_suffix = models.CharField(max_length=64, blank=True, null=True)
 
-	original_price = models.IntegerField(default=0.0) #price in pence. adjust manually once taken
-	#price_suffix = models.CharField(max_length=64) #can make this choices as well for the time?
-
-	modern_pounds = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True, null=True)
-	modern_dollars = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True, null=True)
+	original_price = models.IntegerField(default=0) #price in pence. adjust manually once taken
+	#price_suffix = models.CharField(max_length=64) 
+	modern_pounds = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True, null=True)
+	modern_dollars = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, blank=True, null=True)
 
 	page           = models.IntegerField(blank=True, null=True)
 
@@ -68,6 +67,7 @@ class Product(models.Model):
 	def save(self, *args, **kwargs):
 		self.title_slug = slugify(self.title)
 		super(Product, self).save(*args, **kwargs)
+
 class Ledger(models.Model):
 
 	#foreignkey from customer, called 'customer'
