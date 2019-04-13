31ed34dd08d8bf2ebb6c0fd765ee3225e1e5129f
xunzhou <xzh.dev@gmail.com>
2019-04-12 20:41:58 -0500

    selenium tests mostly done.

--- ramsay/ramsay/test_selenium.py
+++ ramsay/ramsay/test_selenium.py
@@ -1,7 +1,11 @@
-
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 class MySeleniumTests(LiveServerTestCase):
     def setUp(self):
@@ -24,7 +28,6 @@ class MySeleniumTests(LiveServerTestCase):
             '%s%s' % ("http://127.0.0.1:8000",  "/categories/")
         )
         
-        # Category side bar menu
         # @Yu-lin, all failed. I dont believe these are proper css selector?
         '''
         self.assertTrue(selenium.find_elements_by_css_selector('#prodlist'))
@@ -36,16 +39,48 @@ class MySeleniumTests(LiveServerTestCase):
         self.assertTrue(selenium.find_elements_by_css_selector('#foodways'))
         self.assertTrue(selenium.find_elements_by_css_selector('#footwear'))
         '''
-        
-        viewall = selenium.find_element_by_css_selector('a.list-group-item:nth-child(1)').click()
-        craft = selenium.find_element_by_css_selector('a.list-group-item:nth-child(2)').click()
-        argi = selenium.find_element_by_css_selector('a.list-group-item:nth-child(3)').click()
-        tool = selenium.find_element_by_css_selector('a.list-group-item:nth-child(4)').click()
-        textile = selenium.find_element_by_css_selector('a.list-group-item:nth-child(5)').click()
-        grocery = selenium.find_element_by_css_selector('a.list-group-item:nth-child(6)').click()
-        foodway = selenium.find_element_by_css_selector('a.list-group-item:nth-child(7)').click()
-        ftware = selenium.find_element_by_css_selector('a.list-group-item:nth-child(8)').click()
+        # click all category in side bar menu
+        for i in range(len(selenium.find_elements_by_css_selector('a.list-group-item'))):
+            selenium.find_element_by_css_selector('a.list-group-item:nth-child('+str(i+1)+')').click()
+ 
+    def test_viewdetails_clickable(self):
+        sel = self.selenium
+        sel.get("http://127.0.0.1:8000/categories/")
 
-    # def test_addtocart(self):
-        # sel = self.selenium
-        # self.assertTrue(sel.find_elements_by_css_selector('.show'))
+        for r in sel.find_elements_by_class_name('viewdetails'):
+            sel.execute_script("arguments[0].scrollIntoView()", r)
+            # TODO: scrolling to element and wait until r is clickable
+            r.click()
+
+    def test_addtocart_clickable(self):
+        sel = self.selenium
+        sel.get("http://127.0.0.1:8000/categories/")
+
+        for r in sel.find_elements_by_css_selector('#addtocart'):
+            sel.execute_script("arguments[0].scrollIntoView()", r)
+            r.click()
+    
+    def test_cart_clickable(self):
+        sel = self.selenium
+        sel.get("http://127.0.0.1:8000/categories")
+        sel.find_element_by_id('cart').click()
+
+    def test_cart_quantity_and_total(self):
+        sel = self.selenium
+        sel.get("http://127.0.0.1:8000/categories")
+        # TODO: find proper way of ensuring element is clickable.
+        time.sleep(3) 
+        add = sel.find_element_by_css_selector('#addtocart')
+        add.click()
+        time.sleep(1)
+        add.click()
+        time.sleep(1)
+        add.click()
+        time.sleep(1)
+        sel.find_element_by_id('cart').click()
+        time.sleep(1)
+        q = int(sel.find_element_by_css_selector('.cart-row > div:nth-child(2) > div:nth-child(1)').text)
+        p = float(sel.find_element_by_css_selector('.cart-row > div:nth-child(2) > div:nth-child(2)').text)
+        s = sel.find_element_by_css_selector('.text-danger').text
+        self.assertEquals(3, q, "quantity should be 3")
+        self.assertEquals(s, "$"+str(p*q), "subtotal should be $3*8.41 = $25.23")
